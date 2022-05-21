72daa8da13eb9342a75ee4a8a3c47b44e1d1986f
DevTabibur <tobiburrohman2@gmail.com>
2022-05-21 09:29:05 +0600

    contact form done

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,10 +1,54 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Contact.css";
+import {AiOutlineArrowRight} from 'react-icons/ai';
+
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
 
-export default Contact
+  const handleContactForm =(e)=>{
+    e.preventDefault()
+  }
+
+  return (
+    <div className="my-3">
+      <div className="wrapper py-4">
+            <div className="inner">
+              <form onSubmit={handleContactForm}>
+                <h3>Contact Us</h3>
+                <p>
+                  24/7 at your services. Contact Us here.
+                </p>
+                <label className="form-group">
+                  <input type="text" className="form-control" required />
+                  <span>Your Name</span>
+                  <span className="border"></span>
+                </label>
+                <label className="form-group">
+                  <input type="text" className="form-control" required />
+                  <span>Your Mail</span>
+                  <span className="border"></span>
+                </label>
+                <label className="form-group">
+                  <textarea
+                    name=""
+                    id=""
+                    className="form-control"
+                    required
+                  ></textarea>
+                  <span>Your Message</span>
+                  <span className="border"></span>
+                </label>
+                <button  type="submit" className="contact-submit-btn">
+                  CONTACT
+                  <AiOutlineArrowRight className="submit-arrow-icon"/>
+                  <i className="zmdi zmdi-arrow-right"></i>
+                </button>
+              </form>
+            </div>
+          </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import "./Home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
 import HeroBanner from "../Shared/HeroBanner/HeroBanner";
+import Contact from "../Contact/Contact";
 
 const Home = () => {
   return (
     <>
       <HeroBanner />
+      <Contact/>
     </>
   );
 };
