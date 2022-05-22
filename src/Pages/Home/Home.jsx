0bdb8d86576e76975caa70ce2619fbee12705747
DevTabibur <tobiburrohman2@gmail.com>
2022-05-22 15:53:55 +0600

    services icon box done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,11 +4,42 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Col, Container, Row } from "react-bootstrap";
 import HeroBanner from "../Shared/HeroBanner/HeroBanner";
 import Contact from "../Contact/Contact";
+import { AiFillCar, AiOutlineSafetyCertificate } from "react-icons/ai";
+import {MdOutlineSupportAgent} from 'react-icons/md';
+
 
 const Home = () => {
   return (
     <>
-      {/* <HeroBanner /> */}
+      <HeroBanner />
+      <Container className="services py-5">
+        <Row>
+          <Col lg={4} md={4} sm={6} className="">
+            <div className="icon-box d-flex justify-content-center align-items-center">
+              <div className="icon">
+                <AiFillCar className="inner-icon"/>
+              </div>
+              <h2>FREE SHIPPING</h2>
+            </div>
+          </Col>
+          <Col lg={4} md={4} sm={6} className="">
+            <div className="icon-box d-flex justify-content-center align-items-center">
+              <div className="icon">
+                <AiOutlineSafetyCertificate className="inner-icon"/>
+              </div>
+              <h2>SAFE SHIPPING</h2>
+            </div>
+          </Col>
+          <Col lg={4} md={4} sm={6} className="">
+            <div className="icon-box d-flex justify-content-center align-items-center">
+              <div className="icon">
+                <MdOutlineSupportAgent className="inner-icon"/>
+              </div>
+              <h2>ONLINE SUPPORT</h2>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React from "react";
 import "./Footer.css";
 import "bootstrap/dist/css/bootstrap.min.css";
